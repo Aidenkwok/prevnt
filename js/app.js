28aa6249704848ac4f51b867e51fb46d1d3aeb61
jrorrison1 <jr94@live.co.uk>
2017-05-03 20:56:50 +0100

    Colour and text changes

--- js/app.js
+++ js/app.js
@@ -4,19 +4,19 @@ $('#validate').on('input propertychange paste keypress',function() {
 
     for (i = 0; i < database.length; i++) {
         if (x.includes(database[i])) {
-            $(".result").text("this is a genuine email");
+            $(".result").text("THIS IS A GENUINE EMAIL");
             $(".result").css("color","lightgreen");
             y = true;
         }
     }
 
     if (y == false){
-        $(".result").text("this may not be a genuine email");
+        $(".result").text("THIS MAY NOT BE A GENUINE EMAIL");
         $(".result").css("color","red");
     }
 
     if (x == null || x ==""){
-        $(".result").text("Paste your suspected scam email here");
-        $(".result").css("color","white");
+        $(".result").text("PASTE YOUR SUSPECTED SCAM EMAIL ADDRESS IN THE BOX ABOVE TO SEE IF IT IS REAL OR FAKE.");
+        $(".result").css("color","black");
     }
 });
